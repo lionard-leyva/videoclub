ddc81f4f2a7e33a156f193c8663cbe64511837b4
leonard <lionard.cloud@gmail.com>
2024-06-20 06:33:54 -0500

    init

---
+++ src/main/java/com/cleancode/videclub/VideclubApplication.java
@@ -0,0 +1,13 @@
+package com.cleancode.videclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleancode/videclub/VideclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleancode.videclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
